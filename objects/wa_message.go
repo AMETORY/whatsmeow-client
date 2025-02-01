0cbf7ff144f1da9b1048aac2e1a5cfaa014d3c4f
ametsuramet <ametsuramet@gmail.com>
2025-02-01 13:12:19 +0700

    `Added file and image fields to WaMessage struct`

--- objects/wa_message.go
+++ objects/wa_message.go
@@ -1,7 +1,9 @@
 package objects
 
 type WaMessage struct {
-	JID  string `json:"jid"`
-	Text string `json:"text"`
-	To   string `json:"to"`
+	JID   string `json:"jid"`
+	Text  string `json:"text"`
+	File  string `json:"file"`
+	Image string `json:"image"`
+	To    string `json:"to"`
 }
